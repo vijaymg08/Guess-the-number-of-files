1d288a5ff9d2700d9761533c2b7ef982494af780
vijaymg08 <afsvijay@gmail.com>
2021-09-15 15:09:51 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Guess the number of files in the current directory 
+
+Wed Sep 15 14:35:51 IST 2021
+Number of Lines in guessinggame.sh are: 26
---
+++ guessinggame.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+function guessthefiles() {
+
+while true;
+do
+#echo "Please guess the number of files in current working directory"
+read response
+if [ $response -lt $number_of_files ]
+then
+echo "Your guess is LOWER than Actual number. Please Guess Again"
+elif [ $response -gt $number_of_files ]
+then
+echo "Your guess is HIGHER than Actual number. Please Guess Again"
+else
+echo "Congratulations! Your Guess is CORRECT"
+break;
+fi
+done
+}
+
+clear
+
+number_of_files=$(ls . | wc -l)
+echo "Please Guess the number of files in current working directory"
+guessthefiles
